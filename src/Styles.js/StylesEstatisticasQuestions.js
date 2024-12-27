cdaa964b166ffea9d0515322084866c11044c40f
NathanTostes <nathanpt777@gmail.com>
2024-12-27 15:36:09 -0300

    Separate styles from new list stats screen Create css style files apart for new screens.

---
+++ src/Styles.js/StylesEstatisticasQuestions.js
@@ -0,0 +1,83 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    scrollContent: {
+        flexGrow: 1, 
+        justifyContent: "center", 
+        alignItems: "center", 
+        backgroundColor: "rgba(210, 208, 250, 1)", 
+        paddingVertical: 20, 
+      },
+      container: {
+        flex: 1,
+        backgroundColor: "rgba(210, 208, 250, 1)", 
+        alignItems: "center",
+        justifyContent: "center", 
+      },
+      content: {
+        width: 378,
+        backgroundColor: "rgba(210, 208, 250, 1)", 
+        paddingVertical: 20,
+        borderRadius: 10,
+      },
+      card: {
+        width: 329,
+        alignSelf: "center",
+        marginBottom: 20,
+        borderRadius: 10,
+        overflow: "hidden",
+        backgroundColor: "rgba(210, 208, 250, 1)", 
+      },
+      header: {
+        backgroundColor: "#f54f59",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 10,
+      },
+      headerText: {
+        color: "#FFF",
+        fontSize: 14,
+        fontFamily: "Inder-Regular",
+      },
+      errorsHeader: {
+        marginLeft: 40, 
+      },
+      acertosHeader: {
+        marginLeft: 40, 
+      },
+      body: {
+        backgroundColor: "#ffb8bc", 
+        padding: 10,
+        borderBottomLeftRadius: 10, 
+        borderBottomRightRadius: 10, 
+      },
+      bodyRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 5,
+      },
+      bodyText: {
+        color: "#FFF",
+        fontSize: 12,
+        fontFamily: "Inder-Regular",
+      },
+      errorsBody: {
+        marginLeft: 40, 
+      },
+      acertosBody: {
+        marginLeft: 40, 
+        marginRight: 15,
+      },
+      button: {
+        backgroundColor: "#f54f59",
+        borderRadius: 10, 
+        padding: 10,
+        marginTop: 5,
+        alignSelf: "flex-start",
+      },
+      buttonText: {
+        color: "#FFF",
+        fontSize: 12,
+        fontFamily: "Inder-Regular",
+      },
+});
