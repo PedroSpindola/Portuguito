a5cd68a59bd1108b1e26c2279c1efe33c12bb93c
leonohub <mendoncal717@gmail.com>
2025-01-06 18:03:21 -0300

    Created the funcionalitys to estatisticasQuestoes screen.

--- src/Views/EstatisticasQuestoes.js
+++ src/Views/EstatisticasQuestoes.js
@@ -1,74 +1,86 @@
-import React, { useEffect } from "react";  
+import React, { useEffect, useState } from "react";  
 import { View, Text, TouchableOpacity, ScrollView, BackHandler } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useRoute } from "@react-navigation/native";
-import { getAlunosWithList } from "../FuncoesFirebase/ListQuery";
+import { FIREBASE_APP } from "../../FirebaseConfig";
+import { getFirestore, where, collection, query, getDocs } from "firebase/firestore";
 import Styles from "../Styles.js/StylesEstatisticasQuestions";
 
 export default function EstatisticasQuestoes() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
+  const route = useRoute();
+  const id = route.params.itemId;
 
-    const route = useRoute();
-    const id = route.params.itemId;
-
-    useEffect(() => {
-      const onBackPress = () => {
-        navigation.navigate("Listas");
+  const [questoes, setQuestoes] = useState([]);
   
-        return true;
-      };
-    
-      const backHandler = BackHandler.addEventListener(
-        'hardwareBackPress',
-        onBackPress
-      );
-    
-      return () => backHandler.remove();
-    }, []);
+  const loadQuestoes = async () => {
+    loadQuestoes();
+    const db = getFirestore(FIREBASE_APP);
+    const listAlunoRef = collection(db, "ListaAluno");
+    const listAlunoQuery = query(listAlunoRef, where("codigo", "==", id));
+    const listAlunoSnapshot = await getDocs(listAlunoQuery);
 
-  const questoes = [
-    {
-      id: 1,
-      erros: 2,
-      acertos: 3,
-    },
-    {
-      id: 2,
-      erros: 0,
-      acertos: 5,
-    },
-    {
-      id: 3,
-      erros: 5,
-      acertos: 0,
-    },
-  ];
+  
+    let questoesData = {};
 
+    listAlunoSnapshot.forEach((doc) => {
+      const data = doc.data();
+      if (data.acertoQuestoes && Array.isArray(data.acertoQuestoes)) {
+        data.acertoQuestoes.forEach((resultado, index) => {
+          const questaoId = index + 1; 
+          if (!questoesData[questaoId]) {
+            questoesData[questaoId] = { id: questaoId, erros: 0, acertos: 0 };
+          }
+          if (resultado) {
+            questoesData[questaoId].acertos += 1;
+          } else {
+            questoesData[questaoId].erros += 1;
+          }
+        });
+      }
+    });
+
+    const questoesArray = Object.values(questoesData);
+    setQuestoes(questoesArray);
+  };
+
+  useEffect(() => {
+    const onBackPress = () => {
+      navigation.navigate("Listas");
+      return true;
+    };
+
+    
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      onBackPress
+    );
+    
+    return () => backHandler.remove();
+  }, []);
+  
+  
   return (
     <ScrollView contentContainerStyle={Styles.scrollContent}>
       <View style={Styles.container}>
-        <View style={Styles.content}>
-          {[1, 2, 3].map((grupo, index) => (
-            <View key={index} style={Styles.card}>
-              <View style={Styles.header}>
-                <Text style={Styles.headerText}>Questão:</Text>
-                <Text style={[Styles.headerText, Styles.errorsHeader]}>Número de erros</Text>
-                <Text style={[Styles.headerText, Styles.acertosHeader]}>Acertos</Text>
+        <View style={Styles.card}>
+          <View style={Styles.header}>
+            <Text style={Styles.headerText}>Questão</Text>
+            <Text style={[Styles.headerText, Styles.errorsHeader]}>Número de erros</Text>
+            <Text style={[Styles.headerText, Styles.acertosHeader]}>Acertos</Text>
+          </View>
+          <View style={Styles.body}>
+            {questoes.map((questao) => (
+              <View key={questao.id} style={Styles.bodyRow}>
+                <Text style={Styles.bodyText}>Questão {questao.id}</Text>
+                <Text style={[Styles.bodyText, Styles.errorsBody]}>{questao.erros}</Text>
+                <Text style={[Styles.bodyText, Styles.acertosBody]}>{questao.acertos}</Text>
               </View>
-              <View style={Styles.body}>
-                {questoes.map((questao) => (
-                  <View key={questao.id} style={Styles.bodyRow}>
-                    <Text style={Styles.bodyText}>Questão {questao.id}</Text>
-                    <Text style={[Styles.bodyText, Styles.errorsBody]}>{questao.erros}</Text>
-                    <Text style={[Styles.bodyText, Styles.acertosBody]}>{questao.acertos}</Text>
-                  </View>
-                ))}
-              </View>
-              <TouchableOpacity style={Styles.button}>
-                <Text style={Styles.buttonText}>Ver questões</Text>
-              </TouchableOpacity>
-            </View>
-          ))}
+            ))}
+          </View>
+          <TouchableOpacity style={Styles.button}>
+            <Text style={Styles.buttonText}>Ver questões</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </ScrollView>
