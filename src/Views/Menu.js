da1466e737bc6c574f6d8f951d7640fefef23600
leonohub <mendoncal717@gmail.com>
2025-02-08 13:35:48 -0300

    Added the 'criar questoes' and 'questoes feitas por você' buttons

--- src/Views/Menu.js
+++ src/Views/Menu.js
@@ -6,53 +6,88 @@ import { useRoute } from "@react-navigation/native";
 import Styles from "../Styles.js/StylesMenu";
 
 export default function Menu() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const route = useRoute();
-    const id = route.params.itemId;
+  const route = useRoute();
+  const id = route.params.itemId;
 
-    useEffect(() => {
-        const onBackPress = () => {
-            navigation.navigate("Listas");
+  useEffect(() => {
+    const onBackPress = () => {
+      navigation.navigate("Listas");
 
-            return true;
-        };
+      return true;
+    };
 
-        const backHandler = BackHandler.addEventListener(
-            'hardwareBackPress',
-            onBackPress
-        );
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      onBackPress
+    );
 
-        return () => backHandler.remove();
-    }, []);
+    return () => backHandler.remove();
+  }, []);
 
-    const Botao = ({ titulo, onPress }) => (
-        <TouchableOpacity style={Styles.descritores} onPress={onPress} >
-            <Text style={Styles.txtDescritores}>{titulo}</Text>
-        </TouchableOpacity>
-    )
+  const Botao = ({ titulo, onPress }) => (
+    <TouchableOpacity style={Styles.descritores} onPress={onPress}>
+      <Text style={Styles.txtDescritores}>{titulo}</Text>
+    </TouchableOpacity>
+  );
 
-    const BotaoGrande = ({ titulo, onPress }) => (
-        <TouchableOpacity style={Styles.descritoresGrande} onPress={onPress} >
-            <Text style={Styles.txtDescritores}>{titulo}</Text>
-        </TouchableOpacity>
-    )
+  const BotaoGrande = ({ titulo, onPress }) => (
+    <TouchableOpacity style={Styles.descritoresGrande} onPress={onPress}>
+      <Text style={Styles.txtDescritores}>{titulo}</Text>
+    </TouchableOpacity>
+  );
 
-    return (
-        <LinearGradient colors={['#D5D4FB', '#9B98FC']} style={Styles.gradient} >
-            <View style={Styles.container}>
-                <View style={Styles.containerFilho}>
-                    <Botao titulo="Procedimentos de leitura" onPress={() => { navigation.navigate('DescritoresProcedimentoLeitura', { id }) }} />
-                    <Botao titulo="Implicações do gênero textual" onPress={() => { navigation.navigate('DescritorImplicacoesGeneroTextual', { id }); }} />
-                </View>
-                <View style={Styles.containerFilho}>
-                    <Botao titulo="Variação Linguística" onPress={() => { navigation.navigate('DescritorVariacaoLinguistica', { id }) }} />
-                    <Botao titulo="Coerência e Coesão textuais" onPress={() => { navigation.navigate('DescritorCoerenciaCoesaoTextual', { id }) }} />
-                </View>
-                <View style={Styles.containerFilho}>
-                    <BotaoGrande titulo="Relações entre recursos expressivos e efeitos de sentido" onPress={() => { navigation.navigate('DescritorRelacoesEntreRecursosExpressivos', { id }) }} />
-                </View>
-            </View>
-        </LinearGradient>
-    )
+  return (
+    <LinearGradient colors={["#D5D4FB", "#9B98FC"]} style={Styles.gradient}>
+      <View style={Styles.container}>
+        <View style={Styles.containerFilho}>
+          <Botao
+            titulo="Procedimentos de leitura"
+            onPress={() => {
+              navigation.navigate("DescritoresProcedimentoLeitura", { id });
+            }}
+          />
+          <Botao
+            titulo="Implicações do gênero textual"
+            onPress={() => {
+              navigation.navigate("DescritorImplicacoesGeneroTextual", { id });
+            }}
+          />
+        </View>
+        <View style={Styles.containerFilho}>
+          <Botao
+            titulo="Variação Linguística"
+            onPress={() => {
+              navigation.navigate("DescritorVariacaoLinguistica", { id });
+            }}
+          />
+          <Botao
+            titulo="Coerência e Coesão textuais"
+            onPress={() => {
+              navigation.navigate("DescritorCoerenciaCoesaoTextual", { id });
+            }}
+          />
+        </View>
+        <View style={Styles.containerFilho}>
+          <Botao
+            titulo="Relações entre recursos expressivos e efeitos de sentido"
+            onPress={() => {
+              navigation.navigate("DescritorRelacoesEntreRecursosExpressivos", {
+                id,
+              });
+            }}
+          />
+          <Botao
+            titulo="Questões feitas por você"
+            onPress={() => {
+              navigation.navigate("DescritorFeitasPorVoce", {
+                id,
+              });
+            }}
+          />
+        </View>
+      </View>
+    </LinearGradient>
+  );
 }
